b1a3d07dec255ebb0bf09cee1609aa08f65369b3
Mac User <macuser@Macs-MacBook-Air.local>
2024-01-27 11:38:16 -0500

    feat: add web3 context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from "@chakra-ui/react";
+import Web3ContextProvider from '@/contexts/web-3-context';
  
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Web3ContextProvider>
+        <Component {...pageProps} />
+      </Web3ContextProvider>
+    </ChakraProvider>
+  );
 }
