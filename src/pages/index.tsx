68d2042c05c8aafbee4abfe6e60f5e8f543398a3
Mac User <macuser@Macs-MacBook-Air.local>
2024-01-27 11:45:55 -0500

    feat: add basic flow auth for dapps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+import { useMemo, useState } from "react";
+import Head from "next/head";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Icon,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { IWeb3Context, useWeb3Context } from "@/contexts/web-3-context";
+import { FaEthereum } from "react-icons/fa6";
+import { BiLogOut } from "react-icons/bi";
+import Link from "next/link";
 
-const Home: React.FC = () => {
+const BSCTChainID = 97;
+
+export default function Home() {
+  const {
+    connectWallet,
+    disconnect,
+    state: { isAuthenticated, address, currentChain, provider },
+  } = useWeb3Context() as IWeb3Context;
+
   return (
     <div>
-      <h1>Main page</h1>
+      <Head>
+        <title>Mint NFTs</title>
+      </Head>
+      <HStack
+        width="full"
+        as="header"
+        height="80px"
+        px={4}
+        alignItems="center"
+        bg="gray.100"
+      >
+        <HStack as="nav" width="full" justifyContent="space-between">
+          <HStack>
+            {!isAuthenticated ? (
+              <Button
+                onClick={connectWallet}
+                variant="solid"
+                bg="blue.400"
+                colorScheme="blue"
+                gap={2}
+                color="white"
+              >
+                <Icon as={FaEthereum} />
+                Connect wallet
+              </Button>
+            ) : (
+              <Button
+                onClick={disconnect}
+                variant="solid"
+                bg="red.400"
+                colorScheme="red"
+                color="white"
+                gap={2}
+              >
+                <Icon as={BiLogOut} />
+                Disconnect
+              </Button>
+            )}
+          </HStack>
+        </HStack>
+      </HStack>
     </div>
   );
-};
-export default Home;
+}
