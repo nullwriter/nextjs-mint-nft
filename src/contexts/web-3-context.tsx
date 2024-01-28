c3064f21bfd8ab71412c250322695a96e27d2abf
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-27 19:39:40 -0500

    fix: refactor hook

--- src/contexts/web-3-context.tsx
+++ src/contexts/web-3-context.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { createContext, FC, ReactNode, useContext } from "react";
-import useWeb3Provider, { IWeb3State } from "../hooks/useWeb3Provider";
+import useWeb3Provider, { IWeb3State } from "@/hooks/use-web3";
 import { ToastId } from "@chakra-ui/react";
 
 export interface IWeb3Context {
