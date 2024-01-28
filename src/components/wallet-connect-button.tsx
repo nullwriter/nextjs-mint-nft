9dea23e8bc6e81cb80a0534952755fc2910bf3ce
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-27 19:24:11 -0500

    feat: refactor wallet connect btn

---
+++ src/components/wallet-connect-button.tsx
@@ -0,0 +1,55 @@
+import { Button, HStack, Icon } from "@chakra-ui/react";
+import { IWeb3Context, useWeb3Context } from "@/contexts/web-3-context";
+import { FaEthereum } from "react-icons/fa6";
+import { BiLogOut } from "react-icons/bi";
+
+const BSCTChainID = 97;
+
+export default function WalletConnectButton() {
+  const {
+    connectWallet,
+    disconnect,
+    state: { isAuthenticated, address, currentChain, provider },
+  } = useWeb3Context() as IWeb3Context;
+
+  return (
+    <HStack
+        width="full"
+        as="header"
+        height="80px"
+        px={4}
+        alignItems="center"
+        bg="black"
+      >
+        <HStack as="nav" width="full" justifyContent="space-between">
+          <HStack>
+            {!isAuthenticated ? (
+              <Button
+                onClick={connectWallet}
+                variant="solid"
+                bg="blue.400"
+                colorScheme="blue"
+                gap={2}
+                color="white"
+              >
+                <Icon as={FaEthereum} />
+                Connect wallet
+              </Button>
+            ) : (
+              <Button
+                onClick={disconnect}
+                variant="solid"
+                bg="red.400"
+                colorScheme="red"
+                color="white"
+                gap={2}
+              >
+                <Icon as={BiLogOut} />
+                Disconnect
+              </Button>
+            )}
+          </HStack>
+        </HStack>
+      </HStack>
+  );
+}
