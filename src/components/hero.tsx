01e80d3d97ddfece5ff72bb5d708da869bd794d2
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-27 19:24:32 -0500

    feat: initial landing page

---
+++ src/components/hero.tsx
@@ -0,0 +1,18 @@
+
+export default function Hero() {
+  return (
+    <section className="text-black body-font lg:pt-20">
+      <div className="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
+        <div className="flex flex-col w-full mb-2 text-left md:text-center ">
+          <h1 className="mb-2 text-6xl font-bold tracking-tighter text-white lg:text-8xl md:text-7xl">
+            <span>Stunning NFTs </span>
+          </h1>
+          <br></br>
+          <p className="mx-auto  text-xl font-normal leading-relaxed text-gray-600 dark:text-gray-300 lg:w-2/3">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel aliquam diam, eu aliquam quam
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,21 @@
-import { useMemo, useState } from "react";
-import {
-  Box,
-  Button,
-  Heading,
-  HStack,
-  Icon,
-  Input,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
-import { IWeb3Context, useWeb3Context } from "@/contexts/web-3-context";
-import { FaEthereum } from "react-icons/fa6";
-import { BiLogOut } from "react-icons/bi";
-import Link from "next/link";
-
-const BSCTChainID = 97;
+import Hero from '@/components/hero';
+import WalletConnectButton from '@/components/wallet-connect-button';
 
 export default function Home() {
-  const {
-    connectWallet,
-    disconnect,
-    state: { isAuthenticated, address, currentChain, provider },
-  } = useWeb3Context() as IWeb3Context;
-
   return (
     <div>
-      <div className="flex">
-        
-      </div>
-      <HStack
-        width="full"
-        as="header"
-        height="80px"
-        px={4}
-        alignItems="center"
-        bg="gray.100"
-      >
-        <HStack as="nav" width="full" justifyContent="space-between">
-          <HStack>
-            {!isAuthenticated ? (
-              <Button
-                onClick={connectWallet}
-                variant="solid"
-                bg="blue.400"
-                colorScheme="blue"
-                gap={2}
-                color="white"
-              >
-                <Icon as={FaEthereum} />
-                Connect wallet
-              </Button>
-            ) : (
-              <Button
-                onClick={disconnect}
-                variant="solid"
-                bg="red.400"
-                colorScheme="red"
-                color="white"
-                gap={2}
-              >
-                <Icon as={BiLogOut} />
-                Disconnect
-              </Button>
-            )}
-          </HStack>
-        </HStack>
-      </HStack>
+      <Hero />
+      <WalletConnectButton />
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+            Left
+          </div>
+          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+            Right
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
