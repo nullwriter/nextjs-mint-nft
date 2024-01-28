4c0d48bc3495a7c63cd97b394fec12926abda48f
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-27 20:21:54 -0500

    feat: show my nft on auth

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,10 +1,19 @@
+import { IWeb3Context, useWeb3Context } from "@/contexts/web-3-context";
+
 const Nav = () => {
+  const {
+    state: { isAuthenticated },
+  } = useWeb3Context() as IWeb3Context;
+
   return (
     <>
       <div className="md:flex flex-grow items-center flex">
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-semibold pl-7">
           <a className="mr-6 hover:text-white" href="/">Home</a>
           <a className="mr-6 hover:text-white" href="/mint">Mint</a>
+          { isAuthenticated && (
+            <a className="mr-6 hover:text-white" href="/my-nfts">My NFTs</a>
+          )}
         </nav>
       </div>
     </>
