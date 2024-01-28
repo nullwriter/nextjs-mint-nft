7ededecca91f7b13acef0a61cc5f5e88d612ddec
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-27 19:38:18 -0500

    fix: use seo content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,14 @@
 import { FaFacebookF } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
 import { Icon } from '@chakra-ui/react';
+import { seoDefaultContent } from '@/utils/seo';
 
 export default function Footer() {
   return (
     <footer className="text-white body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center" href="/">
-          <span className="ml-3 text-xl">MINT NFTs</span>
+          <span className="ml-3 text-xl">{seoDefaultContent.title}</span>
         </a>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a href="https://facebook.com/">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 import Nav from './nav';
+import { seoDefaultContent } from '@/utils/seo';
 
 export default function Header() {
   return (
     <header className="text-white body-font">
       <div className="container mx-auto flex flex-wrap p-5 md:flex-row">
         <a className="flex title-font font-medium text-white mb-4 md:mb-0 pr-4" href="/">
-          <span className="ml-3 text-3xl">MINT NFTs</span>
+          <span className="ml-3 text-3xl">{seoDefaultContent.title}</span>
         </a>
         <Nav />
       </div>
