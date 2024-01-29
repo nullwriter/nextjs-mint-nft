55100d94e4182d710298efd890dd71e2c7693e7c
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-29 02:28:07 -0500

    fix: remove unused page and nav

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,18 +1,9 @@
-import { IWeb3Context, useWeb3Context } from "@/contexts/web-3-context";
-
 const Nav = () => {
-  const {
-    state: { isAuthenticated },
-  } = useWeb3Context() as IWeb3Context;
-
   return (
     <>
       <div className="md:flex flex-grow items-center flex">
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-semibold pl-7">
           <a className="mr-6 hover:text-white" href="/">Home</a>
-          { isAuthenticated && (
-            <a className="mr-6 hover:text-white" href="/my-nfts">My NFTs</a>
-          )}
         </nav>
       </div>
     </>
