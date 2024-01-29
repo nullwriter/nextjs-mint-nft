2dacd875b6ed409087cd92ed83a80c4584e9b1fe
christianf21 <christianf21@MacBook-Pro-de-Macbook.local>
2024-01-29 02:01:28 -0500

    fix: remove nav page

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -10,7 +10,6 @@ const Nav = () => {
       <div className="md:flex flex-grow items-center flex">
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-semibold pl-7">
           <a className="mr-6 hover:text-white" href="/">Home</a>
-          <a className="mr-6 hover:text-white" href="/mint">Mint</a>
           { isAuthenticated && (
             <a className="mr-6 hover:text-white" href="/my-nfts">My NFTs</a>
           )}
